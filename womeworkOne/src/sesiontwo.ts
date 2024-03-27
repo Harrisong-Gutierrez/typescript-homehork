7b3842a8a8df0a03b1e54d0af23a4c3f5b1cd897
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-03-27 16:30:49 -0600

    deleting node_modules and dist file

--- womeworkOne/src/sesiontwo.ts
+++ womeworkOne/src/sesiontwo.ts
@@ -470,7 +470,7 @@ console.log(logger1 === logger2);
 // a. Crea una clase "Button" con propiedades y métodos para representar un botón en una interfaz de usuario.
 // b. Utiliza la clase "Button" en múltiples lugares de una aplicación para mostrar botones reutilizables con diferentes estilos y funciones.
 // c. Modifica la clase "Button" para agregar nuevas funcionalidades y asegúrate de que los cambios se reflejen en todos los botones utilizados en la aplicación.
-
+//
 
 
 // class Button {
