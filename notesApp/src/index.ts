af6e3b5d9da0ddc9a5596fdc389802394957492b
Harrisong <harrygutie7@gmail.com>
2024-04-04 03:05:06 -0600

    doing homework

--- notesApp/src/index.ts
+++ notesApp/src/index.ts
@@ -1,4 +1,3 @@
-import { Note } from './models/Note';
 import { User } from './models/User';
 import { NotesApp } from './services/ NotesApp';
 
@@ -11,27 +10,26 @@ let arrData: any[] = [];
 
     try {
         const newNote = await notesApp.createNote("Venus", "En la tranquila noche, mis nostalgias amargas sufría. En busca de quietud bajé al fresco y callado jardín. En el obscuro cielo Venus bella temblando lucía, como incrustado en ébano un dorado y divino jazmín. A mi alma enamorada, una reina oriental parecía,que esperaba a su amante bajo el techo de su camarín", user.email, new Date());
-        // console.log("Note created:", newNote);
+        console.log("Note created:", newNote);
         arrData.push(newNote)
 
 
 
         const updatedNote = await notesApp.updateNote(newNote.userId, { description: "New description" });
-        // console.log("Updated note:", updatedNote);
+        console.log("Updated note:", updatedNote);
         arrData.push(updatedNote)
-        // console.log(updatedNote)
 
 
 
         const retrievedNote = await notesApp.getNoteById(updatedNote.userId);
-        // console.log("Recovered note:", retrievedNote);
+        console.log("Recovered note:", retrievedNote);
 
 
 
 
         const deleted = await notesApp.deleteNote(retrievedNote.userId);
-        // console.log("Deleted note:", deleted);
-        // console.log("All notes", arrData)
+        console.log("Deleted note:", deleted);
+   
 
 
 
@@ -41,7 +39,7 @@ let arrData: any[] = [];
 
 
 
-        
+
 
     } catch (error: any) {
         console.error("Error:", error.message);
