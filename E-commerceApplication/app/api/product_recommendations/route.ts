74712dadf6930d98c0fde7d340a83c42452a968f
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-04-11 16:18:36 -0600

    doing the crud

---
+++ E-commerceApplication/app/api/product_recommendations/route.ts
@@ -0,0 +1,54 @@
+import { createClient } from "@/utils/supabase/client";
+import { NextResponse, NextRequest } from "next/server";
+
+const supabase = createClient();
+
+
+
+export const GET = async (request: NextRequest) => {
+    try {
+        const { data: recommendations, error } = await supabase.from("product_recommendations").select("*");
+        if (error) {
+            console.error("Error fetching recommendations:", error.message);
+            return NextResponse.json(
+                { message: "Failed to fetch recommendations", error: error.message },
+                { status: 500 }
+            );
+        }
+        return NextResponse.json(
+            { message: "Recommendations fetched successfully", recommendations },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        console.error("Error fetching recommendations:", error.message);
+        return NextResponse.json(
+            { message: "Failed to fetch recommendations", error: error.message },
+            { status: 500 }
+        );
+    }
+};
+
+
+export const POST = async (request: NextRequest): Promise<NextResponse> => {
+    try {
+        const { id, user_id, product_id, recommendation_date } = await request.json();
+        const { data: recommendations, error } = await supabase.from("product_recommendations").insert([{ id, user_id, product_id, recommendation_date }]);
+        if (error) {
+            console.error("Error adding recommendations:", error.message);
+            return NextResponse.json(
+                { message: "Failed to add recommendations", error: error.message },
+                { status: 500 }
+            );
+        }
+        return NextResponse.json(
+            { message: "recommendations added successfully", recommendations },
+            { status: 201 }
+        );
+    } catch (error: any) {
+        console.error("Error adding recommendations:", error.message);
+        return NextResponse.json(
+            { message: "Failed to add recommendations", error: error.message },
+            { status: 500 }
+        );
+    }
+};
--- E-commerceApplication/app/api/shopping_carts/route.ts
+++ E-commerceApplication/app/api/shopping_carts/route.ts
@@ -31,23 +31,72 @@ export const GET = async (request: NextRequest) => {
 export const POST = async (request: NextRequest): Promise<NextResponse> => {
     try {
         const { id, user_id, creation_date } = await request.json();
-        const { data: user, error } = await supabase.from("shopping_carts").insert([{ id, user_id, creation_date }]);
+        const { data: carts, error } = await supabase.from("shopping_carts").insert([{ id, user_id, creation_date }]);
         if (error) {
-            console.error("Error adding user:", error.message);
+            console.error("Error adding carts:", error.message);
             return NextResponse.json(
-                { message: "Failed to add user", error: error.message },
+                { message: "Failed to add carts", error: error.message },
                 { status: 500 }
             );
         }
 
         return NextResponse.json(
-            { message: "User added successfully", user },
+            { message: "Carts added successfully", carts },
             { status: 201 }
         );
     } catch (error: any) {
-        console.error("Error adding user:", error.message);
+        console.error("Error adding carts:", error.message);
         return NextResponse.json(
-            { message: "Failed to add user", error: error.message },
+            { message: "Failed to add carts", error: error.message },
+            { status: 500 }
+        );
+    }
+};
+
+export const PUT = async (request: NextRequest): Promise<NextResponse> => {
+    try {
+        const { id, user_id, creation_date } = await request.json();
+        const { data: carts, error } = await supabase.from("shopping_carts").update({ id, user_id, creation_date }).eq('id', id);
+        if (error) {
+            console.error("Error updating carts:", error.message);
+            return NextResponse.json(
+                { message: "Failed to update carts", error: error.message },
+                { status: 500 }
+            );
+        }
+        return NextResponse.json(
+            { message: "Carts updated successfully", carts },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        console.error("Error updating carts:", error.message);
+        return NextResponse.json(
+            { message: "Failed to update carts", error: error.message },
+            { status: 500 }
+        );
+    }
+};
+
+
+export const DELETE = async (request: NextRequest): Promise<NextResponse> => {
+    try {
+        const { id } = await request.json();
+        const { error } = await supabase.from("shopping_carts").delete().eq('id', id);
+        if (error) {
+            console.error("Error deleting carts:", error.message);
+            return NextResponse.json(
+                { message: "Failed to delete carts", error: error.message },
+                { status: 500 }
+            );
+        }
+        return NextResponse.json(
+            { message: "Carts deleted successfully" },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        console.error("Error deleting carts:", error.message);
+        return NextResponse.json(
+            { message: "Failed to delete carts", error: error.message },
             { status: 500 }
         );
     }
