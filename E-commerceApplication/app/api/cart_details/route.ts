925665965ef03264839e8fbc1a614f1f3052d5b4
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-04-12 10:15:14 -0600

    purchase_history

---
+++ E-commerceApplication/app/api/cart_details/route.ts
@@ -0,0 +1,55 @@
+import { createClient } from "@/utils/supabase/client";
+import { NextResponse, NextRequest } from "next/server";
+
+const supabase = createClient();
+
+
+
+export const GET = async (request: NextRequest) => {
+    try {
+        const { data: cartDetails, error } = await supabase.from("cart_details").select("*");
+        if (error) {
+            console.error("Error fetching cart Details:", error.message);
+            return NextResponse.json(
+                { message: "Failed to fetch cart Details", error: error.message },
+                { status: 500 }
+            );
+        }
+        return NextResponse.json(
+            { message: "CartDetails fetched successfully", cartDetails },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        console.error("Error fetching cart Details:", error.message);
+        return NextResponse.json(
+            { message: "Failed to fetch cart Details", error: error.message },
+            { status: 500 }
+        );
+    }
+};
+
+
+
+export const DELETE = async (request: NextRequest): Promise<NextResponse> => {
+    try {
+        const { id } = await request.json();
+        const { error } = await supabase.from("cart_details").delete().eq('id', id);
+        if (error) {
+            console.error("Error deleting cart Details:", error.message);
+            return NextResponse.json(
+                { message: "Failed to delete cart Details", error: error.message },
+                { status: 500 }
+            );
+        }
+        return NextResponse.json(
+            { message: "cartDetails History deleted successfully" },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        console.error("Error deleting cart Details:", error.message);
+        return NextResponse.json(
+            { message: "Failed to delete  cart Details", error: error.message },
+            { status: 500 }
+        );
+    }
+};
