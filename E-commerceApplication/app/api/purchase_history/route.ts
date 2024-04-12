b46aeafee52fa95b357a0229855eb249105bb22b
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-04-12 09:20:23 -0600

    purchase_history

---
+++ E-commerceApplication/app/api/purchase_history/route.ts
@@ -0,0 +1,53 @@
+import { createClient } from "@/utils/supabase/client";
+import { NextResponse, NextRequest } from "next/server";
+
+const supabase = createClient();
+
+
+export const GET = async (request: NextRequest) => {
+    try {
+        const { data: purchaseHistory, error } = await supabase.from("purchase_history").select("*");
+        if (error) {
+            console.error("Error fetching purchaseHistory:", error.message);
+            return NextResponse.json(
+                { message: "Failed to fetch purchaseHistory", error: error.message },
+                { status: 500 }
+            );
+        }
+        return NextResponse.json(
+            { message: "PurchaseHistory fetched successfully", purchaseHistory },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        console.error("Error fetching purchaseHistory:", error.message);
+        return NextResponse.json(
+            { message: "Failed to fetch purchaseHistory", error: error.message },
+            { status: 500 }
+        );
+    }
+};
+
+
+export const POST = async (request: NextRequest): Promise<NextResponse> => {
+    try {
+        const { id, user_id, product_id, purchase_date } = await request.json();
+        const { data: purchaseHistory, error } = await supabase.from("purchase_history").insert([{ id, user_id, product_id, purchase_date }]);
+        if (error) {
+            console.error("Error adding purchase History:", error.message);
+            return NextResponse.json(
+                { message: "Failed to add purchase History", error: error.message },
+                { status: 500 }
+            );
+        }
+        return NextResponse.json(
+            { message: "PurchaseHistory added successfully", purchaseHistory },
+            { status: 201 }
+        );
+    } catch (error: any) {
+        console.error("Error adding purchase History:", error.message);
+        return NextResponse.json(
+            { message: "Failed to add purchase History", error: error.message },
+            { status: 500 }
+        );
+    }
+};
