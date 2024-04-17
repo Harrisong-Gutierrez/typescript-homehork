a11e34cbd0e9fa5d015dfb4422ee57d8268066b2
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-04-17 13:47:24 -0600

    fixing womework 5 Tipos Condicionales e Inferidos

--- PersonnelManagementSystem/src/employeeManager.ts
+++ PersonnelManagementSystem/src/employeeManager.ts
@@ -1,24 +1,4 @@
 
-
-// import { Employee, EmployeeWithSalary } from './types';
-// import { validateName } from './decorators';
-
-// export class EmployeeManager {
-//     employees: (Employee | EmployeeWithSalary)[] = [];
-
-//     @validateName
-//     addEmployee(employee: Employee | EmployeeWithSalary) {
-//         this.employees.push(employee);
-//     }
-
-//     calculateTotalSalary(): number {
-
-//         const employeesWithSalary = this.employees.filter(
-//             (employee): employee is EmployeeWithSalary => 'salary' in employee
-//         );
-//         return employeesWithSalary.reduce((total, employee) => total + employee.salary, 0);
-//     }
-// }
 import { validateName } from './decorators';
 import { Employee, EmployeeWithSalary } from './types';
 
--- PersonnelManagementSystem/src/index.ts
+++ PersonnelManagementSystem/src/index.ts
@@ -1,26 +1,3 @@
-// import { EmployeeManager, loadEmployees } from './employees';
-// import { EmployeeWithSalary } from './types';
-
-
-// const manager = new EmployeeManager();
-
-
-// const employeeWithSalary: EmployeeWithSalary = {
-//     name: 'Juan Gutierrez',
-//     age: 30,
-//     salary: 50000
-// };
-
-
-// manager.addEmployee(employeeWithSalary);
-
-// console.log(manager.calculateTotalSalary());
-
-
-// loadEmployees().then(() => {
-//     console.log(manager.employees);
-// });
-
 import { EmployeeManager, loadEmployees } from './fileManager';
 import { EmployeeWithSalary } from './types';
 
